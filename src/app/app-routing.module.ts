8391b8381e471c866d672b71d2496daac42bdacb
willy[MAC] <w.petitmaire@gmail.com>
2020-06-28 17:35:48 +0200

    Création composant Webcam + importation materialize

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WebcamComponent } from './webcam/webcam.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'webcam', component: WebcamComponent },
+  { path: '', component: WebcamComponent },
+  { path: '**', component: WebcamComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
